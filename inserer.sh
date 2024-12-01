185e31ebae0226c1da6757745ccd6596b0427bb3
JC <jean-charles.kaing@etu.sorbonne-universite.fr>
2024-12-01 19:43:10 +0100

    Ajout de inserer.sh

---
+++ inserer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <x> <y>"
+    exit 1
+fi
+
+x=$1
+y=$2
+
+# Check if x is either 0 or 1
+if [ "$x" -eq 1 ]; then
+    echo "Running inserer_patricia.c with arguument $y"
+    # ./inserer_patricia "$y"
+elif [ "$x" -eq 0 ]; then
+    echo "Running inserer_trie.c with argument $y"
+    ./inserer_trie "$y"
+else
+    echo "Error: x must be 0 or 1"
+    exit 1
+fi
