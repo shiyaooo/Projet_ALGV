418339bf8d0f374a3f0f56bf276176072ab629b5
Jean-Charles Kaing <jean-charles.kaing@etu.sorbonne-universite.fr>
2024-12-16 15:39:39 +0100

    fusion.sh

--- fusion.sh
+++ fusion.sh
@@ -16,8 +16,8 @@ if [ "$x" -eq 0 ]; then
     echo "Running prefixe_patricia.c with argument $y"
     ./fusion_patricia "$y" "$z"
 elif [ "$x" -eq 1 ]; then
-    echo "Running prefixe_trie.c with argument $y"
-    #./fusion_patricia "$y" "$z"
+    echo "Running fusion_trie.c with argument $y"
+    #./fusion_trie "$y" "$z"
 else
     echo "Error: x must be 0 or 1"
     exit 1
